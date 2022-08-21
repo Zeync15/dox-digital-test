10392b83666080c662d53471cef4a47edef3cd88
Ze Yen <zeyenchai@gmail.com>
2022-08-21 12:40:51 +0800

    add content boxes

--- components/Content.js
+++ components/Content.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import LinesEllipsis from "react-lines-ellipsis";
 
 const Content = () => {
-  return (
-    <div>Content</div>
-  )
-}
+  const lorem30 =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae esse nam temporibus tempora numquam ab minus ullam maiores quam nemo adipisci, repellat, tempore quaerat distinctio amet placeat aperiam laudantium.";
 
-export default Content
+  return (
+    <div className="mx-4 lg:w-[1200px] lg:mx-auto">
+      <h2 className="uppercase text-5xl text-red-600 mt-10 mb-28 underline underline-offset-[30px] decoration-2 decoration-amber-400">
+        h2 <span className="font-bold">headline</span>
+      </h2>
+
+      <div className="flex flex-col gap-4  lg:flex-row lg:justify-between lg:gap-0">
+        <div className="bg-red-600 aspect-video lg:w-[360px] lg:h-[360px]">
+          <h5 className="flex flex-col justify-center items-center h-full p-4 text-white text-center">
+            <LinesEllipsis
+              text={lorem30}
+              maxLine={3}
+              ellipsis={"..."}
+              trimRight
+              basedOn={"letters"}
+            />
+            <button className="uppercase mt-8 border-2 rounded-full py-3 px-5 font-bold hover:text-red-600 hover:bg-white">
+              Find jobs
+            </button>
+          </h5>
+        </div>
+
+        <div className="bg-red-600 aspect-video lg:w-[360px] lg:h-[360px]">
+          <h5 className="flex flex-col justify-center items-center h-full p-4 text-white text-center">
+            <LinesEllipsis
+              text={lorem30}
+              maxLine={3}
+              ellipsis={"..."}
+              trimRight
+              basedOn={"letters"}
+            />
+            <button className="uppercase mt-8 border-2 rounded-full py-3 px-5 font-bold hover:text-red-600 hover:bg-white">
+              Find jobs
+            </button>
+          </h5>
+        </div>
+
+        <div className="bg-red-600 aspect-video lg:w-[360px] lg:h-[360px]">
+          <h5 className="flex flex-col justify-center items-center h-full p-4 text-white text-center">
+            <LinesEllipsis
+              text={lorem30}
+              maxLine={3}
+              ellipsis={"..."}
+              trimRight
+              basedOn={"letters"}
+            />
+            <button className="uppercase mt-8 border-2 rounded-full py-3 px-5 font-bold hover:text-red-600 hover:bg-white">
+              Find jobs
+            </button>
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
--- components/Hero.js
+++ components/Hero.js
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
     <div className="">
       <img src="/1920x1080.png" alt="bg" className="max-w-full  lg:hidden h-[720px]" />
-      <img src="/3840x2160.png" alt="bg" className="max-w-full hidden lg:block lg:h-[1080px] " />
+      <img src="/3840x2160.png" alt="bg" className="max-w-full hidden lg:block lg:h-[1080px]" />
 
       <div className="absolute top-20 lg:top-52 left-10 lg:left-20 text-white mr-10 md:mr-0 mt-24">
-        <div className="max-w-[720px] lg:max-w-[1080px]s">
+        <div className="max-w-[720px] lg:max-w-[1080px]">
           <h1 className="text-5xl">
             H1 <span className="font-bold">HEADLINE</span>
           </h1>
@@ -20,7 +20,7 @@ const Hero = () => {
           </p>
 
           <div className="flex flex-col lg:flex-row mt-4 text-white lg:gap-6 mx-3 lg:mx-0">
-            <button className="py-1 px-8 border rounded-full ">
+            <button className="py-1 px-8 border rounded-full">
               <Link href="#">BUTTON 1</Link>
             </button>
             <button className="py-1 px-8 border rounded-full mt-3 lg:mt-0">
